7b5703f86ed39006458326acb260d6236edd6864
m-sowraj <140877454+m-sowraj@users.noreply.github.com>
2025-02-21 21:45:53 +0530

    update

--- src/controllers/Partners/productController.js
+++ src/controllers/Partners/productController.js
@@ -17,12 +17,6 @@ const createProduct = async (req, res) => {
 const updateProduct = async (req, res) => {
     const updates = Object.keys(req.body);
 
-    const allowedUpdates = ['name', 'description', 'price', 'discounted_price', 'images', 'is_active', 'is_deleted'];
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).json({ error: 'Invalid updates!' });
-    }
 
     try {
         const product = await Product.findById(req.params.id);
